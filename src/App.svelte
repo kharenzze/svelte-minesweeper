1e2ffe617e29a13cd042ff486afb5f1e4a16c38f
Paolo <str237@gmail.com>
2021-07-21 00:27:29 +0200

    Rendering matrix

--- src/App.svelte
+++ src/App.svelte
@@ -1,32 +1,47 @@
 <script lang="ts">
-	import Cell from './Cell.svelte'
-	export let name: string;
+  import Cell from './Cell.svelte'
+
+  export let name: string
+  let matrix = [
+    [1, 2, 3],
+    [1, 2, 3],
+    [1, 2, 3],
+  ]
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-    <Cell number="1"/>
-	<Cell number="2"/>
+    <h1>Hello {name}!</h1>
+    <table>
+        <tbody>
+        {#each matrix as row}
+            <tr>
+                {#each row as num}
+                    <Cell number="{num}"/>
+                {/each}
+            </tr>
+        {/each}
+        </tbody>
+    </table>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
 </style>
--- src/Cell.svelte
+++ src/Cell.svelte
@@ -2,9 +2,9 @@
     export let number: Number = 1
 </script>
 
-<div class="{`container number-${number}`}">
+<td class="{`container number-${number}`}">
     {number}
-</div>
+</td>
 
 <style>
     :root {
@@ -16,11 +16,9 @@
         width: var(--cell-size);
         background-color: #999999;
         border: grey solid 1px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
         font-weight: bolder;
         box-sizing: border-box;
+        margin: 0;
     }
 
     .number-1 {
