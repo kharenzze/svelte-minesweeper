b29a952af0c2345b6b45931863c2a3621cffa9e5
Paolo <str237@gmail.com>
2021-07-20 23:58:40 +0200

    Define component: Cell

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+	import Cell from './Cell.svelte'
 	export let name: string;
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+    <Cell/>
 </main>
 
 <style>
---
+++ src/Cell.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+</script>
+
+<div class="container">
+    hi
+</div>
+
+<style>
+    .container {
+        color: red;
+    }
+</style>
