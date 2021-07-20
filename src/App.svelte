968f7ab2fb974eb4c5276ded9d77833d1a2e0164
Paolo <str237@gmail.com>
2021-07-21 00:18:32 +0200

    Remove example text

--- src/App.svelte
+++ src/App.svelte
@@ -5,7 +5,6 @@
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
     <Cell number="1"/>
 	<Cell number="2"/>
 </main>
