443cfbd55aefdbb52f8107d88d1fe10dbf3eb9cf
Paolo <str237@gmail.com>
2021-07-21 22:57:55 +0200

    More point fns

--- src/Point.ts
+++ src/Point.ts
@@ -1,4 +1,13 @@
 export interface Point {
-  x: Number,
-  y: Number,
+  x: number,
+  y: number,
+}
+
+export const Point = (x: number, y: number): Point => ({x, y})
+
+export const PointHelper = {
+  isPositive: (p: Point) =>
+    p.x >= 0 && p.y >= 0,
+
+  area: (p: Point) => p.x * p.y,
 }
