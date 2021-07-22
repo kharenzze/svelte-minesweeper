85a7544eec2a9a1c80572b627eea5cd9253c50f8
Paolo <str237@gmail.com>
2021-07-22 22:43:57 +0200

    Some point fns

--- src/Point.ts
+++ src/Point.ts
@@ -3,15 +3,31 @@ export interface Point {
   y: number,
 }
 
-export interface BoundedPoint extends Point {
-  bounds: Point
-}
-
 export const Point = (x: number, y: number): Point => ({x, y})
+
+const getAroundBuilder = (diff: Array<number>) => (p: Point): Array<Point> => {
+  const points = new Array(9)
+  diff.forEach(i => {
+    diff.forEach(j => {
+      const q = Point(i, j)
+      if (!PointHelper.isZero(q))
+        points.push(PointHelper.add(p, q))
+    })
+  })
+  return points
+}
 
 export const PointHelper = {
   isPositive: (p: Point) =>
     p.x >= 0 && p.y >= 0,
 
+  isZero: (p: Point) => p.x === 0 && p.y === 0,
+
   area: (p: Point) => p.x * p.y,
+
+  getPointsAround: getAroundBuilder([-1, 0, 1]),
+
+  getPointsAdjacent: getAroundBuilder([-1, 1]),
+
+  add: (p: Point, q: Point): Point => Point(p.x + q.x, p.y + q.y),
 }
